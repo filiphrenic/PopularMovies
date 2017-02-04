4b1723d7bfc505a99e36b846d0628ad43c7af25e
Filip Hrenić <hrenic.filip@gmail.com>
2017-02-04 18:55:29 +0100

    Add sort-popup menu to home activity

--- app/src/main/java/com/hrenic/popularmovies/HomeActivity.java
+++ app/src/main/java/com/hrenic/popularmovies/HomeActivity.java
@@ -1,7 +1,12 @@
 package com.hrenic.popularmovies;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.PopupMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -10,4 +15,62 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    /**
+     * Sort movies by most popular first
+     */
+    private void sortByMostPopular() {
+        // TODO
+    }
+
+    /**
+     * Sort movies by top rated first
+     */
+    private void sortByTopRated() {
+        // TODO
+    }
+
+    /**
+     * Show popup menu for sort criteria
+     *
+     * @param item item that was clicked
+     */
+    public void showSortPopup(MenuItem item) {
+
+        View sortItem = findViewById(R.id.action_sort);
+        PopupMenu popup = new PopupMenu(this, sortItem);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.sort_popup, popup.getMenu());
+        popup.show();
+
+        popup.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()) {
+
+                    case R.id.sort_by_most_popular:
+                        HomeActivity.this.sortByMostPopular();
+                        break;
+
+                    case R.id.sort_by_top_rated:
+                        HomeActivity.this.sortByTopRated();
+                        break;
+
+                    default:
+                        return false;
+
+                }
+                return true;
+            }
+
+        });
+    }
+
 }
