92903bda80d6edd2d1e37ce59b676f5f3f23348f
Filip Hrenić <hrenic.filip@gmail.com>
2017-02-06 21:00:44 +0100

    Movie implements Parcelable

--- app/src/main/java/com/hrenic/popularmovies/model/Movie.java
+++ app/src/main/java/com/hrenic/popularmovies/model/Movie.java
@@ -1,5 +1,8 @@
 package com.hrenic.popularmovies.model;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import com.hrenic.popularmovies.util.Config;
 
 import org.json.JSONException;
@@ -9,12 +12,11 @@ import org.json.JSONObject;
  * This class represents a movie retrieved from the Movie DB
  */
 
-public class Movie {
+public class Movie implements Parcelable {
 
     /*
         JSON keys
      */
-
     private static final String ORIGINAL_TITLE_KEY = "original_title";
     private static final String POSTER_URL_KEY = "poster_path";
     private static final String PLOT_SYNOPSIS_KEY = "overview";
@@ -38,6 +40,40 @@ public class Movie {
             throw new IllegalArgumentException(ex);
         }
     }
+
+    private Movie(Parcel source) {
+        originalTitle = source.readString();
+        posterURL = source.readString();
+        plotSynopsis = source.readString();
+        userRating = source.readDouble();
+        releaseDate = source.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(originalTitle);
+        dest.writeString(posterURL);
+        dest.writeString(plotSynopsis);
+        dest.writeDouble(userRating);
+        dest.writeString(releaseDate);
+    }
+
+    public static final Parcelable.Creator<Movie> CREATOR = new Creator<Movie>() {
+        @Override
+        public Movie createFromParcel(Parcel source) {
+            return new Movie(source);
+        }
+
+        @Override
+        public Movie[] newArray(int size) {
+            return new Movie[size];
+        }
+    };
 
     public String getFullPosterURL() {
         return Config.MOVIE_DB_POSTER_BASE_URL + Config.DEFAULT_IMAGE_SIZE + posterURL;
