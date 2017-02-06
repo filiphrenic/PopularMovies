aad2e3816870ae523988cec6de175db1785ba57f
Filip Hrenić <hrenic.filip@gmail.com>
2017-02-06 16:31:42 +0100

    Create empty MovieActivity

---
+++ app/src/main/java/com/hrenic/popularmovies/activities/MovieActivity.java
@@ -0,0 +1,15 @@
+package com.hrenic.popularmovies.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.hrenic.popularmovies.R;
+
+public class MovieActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie);
+    }
+}
